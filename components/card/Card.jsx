7b27bb3c7d0513ae7dc44dbfce24e175ca3cfeb7
TheBraveFrog <113203035+TheBraveFrog@users.noreply.github.com>
2023-11-14 14:56:19 -0700

    better card

--- components/card/Card.jsx
+++ components/card/Card.jsx
@@ -4,10 +4,10 @@ import {BsCart2} from "react-icons/bs"
 
 function Card ({id, artist, label, producer, title, description }){
     return (
-           <div class="container mx-auto p-6 flex">
-                  <div class="flex items-stretch mx-4">
-                         <aside class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 flex flex-col">
-                          <div className='p-3'>
+           <div class="p-6 flex">
+                  <div class="flex">
+                         <aside class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 flex flex-wrap">
+                          <div>
                                  <h2>Album Title: {title}</h2>
                                  <p>Artist: {artist}</p>
                                  <p>Producer: {producer}</p>
