7f653c5fcbbc9a12a1a14deb70f4323d6b3ecdb7
TheBraveFrog <113203035+TheBraveFrog@users.noreply.github.com>
2023-11-27 15:21:37 -0700

    change

--- app/record/[id]/page.js
+++ app/record/[id]/page.js
@@ -3,26 +3,31 @@ import Link from 'next/link'
 import { PageHeader } from "../../../components/header/PageHeader"
 import {NavBar} from "../../../components/navigation/NavBar"
 import {SingleRecordCard} from "../../../components/card/SingleRecordCard"
+import { getRecords } from "@/lib/firebase/getRecords"
+
+async function getRecords(id) {
+  const payload = await getRecords()
+  const records = Object.values(payload)
+  const singleRecord = records.find(item => item.id == id)
+  return singleRecord
+}
  
 
- async function RecordPage () {
+ async function RecordPage ({params}) {
+  const id = params.id
+  const recordData = await getRecords(id)
  
-
-  
-
+ 
    return(
     <>
     <NavBar/>
     <PageHeader title="Single Record Display" tagline=""/>
     <main className="py-6 flex flex-wrap justify-center">
-      {
-        values.map(item=> <SingleRecordCard key={item.id} {...item}/>)
-      }
-     </main>
+     <SingleRecordCard/>
+    </main>
      <Footer/>
     </>
    )
-  
- }
+}
 
 export default RecordPage
