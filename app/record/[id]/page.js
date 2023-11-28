4b907dd7320a983a12ed51a001f2026ac7c2abb0
TheBraveFrog <113203035+TheBraveFrog@users.noreply.github.com>
2023-11-28 11:43:54 -0700

    back

--- app/record/[id]/page.js
+++ app/record/[id]/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import { PageHeader } from "../../../components/header/PageHeader"
 import { Footer } from "../../../components/footer/Footer"
 import {NavBar} from "../../../components/navigation/NavBar"
-//import {SingleRecordCard} from "../../../components/card/SingleRecordCard"
+import {SingleRecordCard} from "../../../components/card/SingleRecordCard"
 import {getRecords} from "@/lib/firebase/getRecords"
 
 async function getRecord(id) {
@@ -24,18 +24,7 @@ async function getRecord(id) {
     <NavBar/>
     <PageHeader title="Single Record Display" tagline="Check out more info"/>
     <main className="py-6 flex flex-wrap justify-center">
-    <aside  class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 flex">
-           <h2>{title}</h2>
-            <p>{artist}</p>
-            <p>{producer}</p>
-            <p>{label}</p>
-            <p>{release_date}</p>
-            <p>{description}</p>
-            <p>{tracks}</p>
-            <p>{sales}</p>
-            <p>{rating}</p>
-            <Link className='text-blue-600 font-semibold' href={`/records`}>Back to Records Page</Link>
-           </aside>
+     <SingleRecordCard/>
     </main>
      <Footer/>
     </>
