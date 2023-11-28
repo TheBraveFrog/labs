0233931b572b69936d20cc33fefd7b931a351cb5
TheBraveFrog <113203035+TheBraveFrog@users.noreply.github.com>
2023-11-28 13:14:23 -0700

    no flex

--- components/card/SingleRecordCard.jsx
+++ components/card/SingleRecordCard.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 function SingleRecordCard ({artist, description, label, producer, rating, release_date, sales, title, tracks }){
 
     return (
-           <aside  class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 flex">
+           <aside  class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
            <h2>{title}</h2>
             <p>{artist}</p>
             <p>{producer}</p>
