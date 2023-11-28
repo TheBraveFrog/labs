452da0f870e16dead4686bbe6da2b8d836e75676
TheBraveFrog <113203035+TheBraveFrog@users.noreply.github.com>
2023-11-28 13:16:30 -0700

    more detail

--- components/card/SingleRecordCard.jsx
+++ components/card/SingleRecordCard.jsx
@@ -5,15 +5,15 @@ function SingleRecordCard ({artist, description, label, producer, rating, releas
 
     return (
            <aside  class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-           <h2>{title}</h2>
-            <p>{artist}</p>
-            <p>{producer}</p>
-            <p>{label}</p>
-            <p>{release_date}</p>
+           <h2>Title:{title}</h2>
+            <p>Artist:{artist}</p>
+            <p>Producer:{producer}</p>
+            <p>Label:{label}</p>
+            <p>Release Date:{release_date}</p>
             <p>{description}</p>
-            <p>{tracks}</p>
-            <p>{sales}</p>
-            <p>{rating}</p>
+            <p>Tracks:{tracks}</p>
+            <p>Number Sold:{sales}</p>
+            <p>Rating:{rating}</p>
             <Link className='text-blue-600 font-semibold' href={`/records`}>Back to Records Page</Link>
            </aside>
     )
