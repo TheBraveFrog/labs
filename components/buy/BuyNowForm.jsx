aed7a7ab2fe4afc60134181c6986739e4139ba39
Anthony <tonypedro59@gmail.com>
2023-12-13 14:52:31 -0700

    pleaae

--- components/buy/BuyNowForm.jsx
+++ components/buy/BuyNowForm.jsx
@@ -1,32 +1,21 @@
-function BuyNow({priceId}) {
+function BuyNow({ priceId }) {
     return (
-    <form
-    action="app/api/checkout"
-    method="POST"
-    >
-    <input
-    type="hidden"
-    name="id"
-    value="from dashboard"
-    />
-    {/* ADD THE PRICE ID TO THE HIDDEN FIELD */}
-    <input
-    type="hidden"
-    name="price"
-    value={priceId}
-    />
-    <button
-    type="submit"
-    value="submit"
-    className="w-2/5 text-xl font-bold rounded-md bg-white/5 hover:bg-white/20
-    
-    py-2.5 text-black ring-4 ring-white/5 ring-offset-2
-    ring-offset-white/[0.09]"
-    
-    >
-    Buy Now
-    </button>
-    </form>
+      <form action="/api/checkout" method="POST">
+        <input type="hidden" name="id" value="from dashboard" />
+        {/* ADD THE PRICE ID TO THE HIDDEN FIELD */}
+        <input type="hidden" name="price" value={priceId} />
+        <button
+          type="submit"
+          value="submit"
+          className="w-2/5 text-xl font-bold rounded-md bg-white/5 hover:bg-white/20
+          py-2.5 text-black ring-4 ring-white/5 ring-offset-2
+          ring-offset-white/[0.09]"
+        >
+          Buy Now
+        </button>
+      </form>
     );
-    }
-    export { BuyNow };
+  }
+  
+  export { BuyNow };
+  
