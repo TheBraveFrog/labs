c6f68702e9fce0941792b44917fe149652d1e7c5
Anthony <tonypedro59@gmail.com>
2023-12-13 14:20:20 -0700

    work

--- components/buy/BuyNowForm.jsx
+++ components/buy/BuyNowForm.jsx
@@ -1,32 +1,57 @@
-function BuyNow({priceId}) {
-    return (
-    <form
-    action="app/api/checkout"
-    method="POST"
-    >
-    <input
-    type="hidden"
-    name="id"
-    value="from dashboard"
-    />
-    {/* ADD THE PRICE ID TO THE HIDDEN FIELD */}
-    <input
-    type="hidden"
-    name="price"
-    value={priceId}
-    />
-    <button
-    type="submit"
-    value="submit"
-    className="w-2/5 text-xl font-bold rounded-md bg-white/5 hover:bg-white/20
-    
-    py-2.5 text-black ring-4 ring-white/5 ring-offset-2
-    ring-offset-white/[0.09]"
-    
-    >
-    Buy Now
-    </button>
-    </form>
-    );
+// pages/api/checkout/[...nextauth].js
+
+import { stripe } from "@/lib/stripe";
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const body = await req.formData();
+      const formData = new FormData();
+      const price = body.get("price");
+      const quantity = 1;
+
+      // Create Checkout Sessions from body params.
+      const checkoutSession = await stripe.checkout.sessions.create({
+        mode: "payment",
+        line_items: [
+          {
+            price: '{{price_1OMGQ5Hk25vonLhFO95PiF3n}}',
+            quantity: 1,
+          },
+          {
+            price: '{{price_1OMGdYHk25vonLhFrj69oJ5r}}',
+            quantity: 1,
+          },
+          {
+            price: '{{price_1OMGhNHk25vonLhFhdsmUDLP}}',
+            quantity: 1,
+          },
+          {
+            price: '{{price_1OMGiLHk25vonLhFO0zKwAZP}}',
+            quantity: 1,
+          },
+          {
+            price: '{{price_1OMGjHHk25vonLhFf9DSvziH}}',
+            quantity: 1,
+          },
+          {
+            price: '{{price_1OMGndHk25vonLhFWed6RY2J}}',
+            quantity: 1,
+          },
+        ],
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
+        automatic_tax: { enabled: true },
+      });
+
+      return res.redirect(checkoutSession.url);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Internal Server Error" });
     }
-    export { BuyNow };
+  } else {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
+}
+
+
